9e302f91346fbb0faa23c48a2d62351d857a4ed0
davfry <davecfryer@gmail.com>
2016-08-31 16:17:03 -0600

    missing semicolon fix

--- src/js/components/MainTripBrick.js
+++ src/js/components/MainTripBrick.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Bricks from 'grommet/components/Bricks';
 import TripBrickModule from './TripBrickModule';
 
-import trips from '../../data/trips.js'
+import trips from '../../data/trips.js';
 
 
 export default class MainTripBrick extends Component {
