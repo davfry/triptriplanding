13b7b27e120e1775ac378157e89883d770128cf2
davfry <davecfryer@gmail.com>
2016-08-31 17:35:27 -0600

    added placeholders

--- src/js/components/TripBrickModule.js
+++ src/js/components/TripBrickModule.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import Brick from 'grommet/components/Brick';
 import Box from 'grommet/components/Box';
+import Button from 'grommet/components/Button';
 
 import Favorite from 'grommet/components/icons/base/Favorite';
-import Edit from 'grommet/components/icons/base/Edit';
 
 export default class TripBrickModule extends Component {
   render() {
     return (
-        <Brick label={this.props.tripName} colorIndex="accent-1" type="medium">
+        <Brick label={this.props.tripName} colorIndex="accent-1" type="medium" texture={this.props.picture} >
           <Box justify="between" direction="row">
-            <Favorite onClick={true}/>
-            <Edit onClick={true} align="end"/>
+            <Button icon={<Favorite />} onClick={true} label={this.props.likes} plain={true} />
           </Box>
         </Brick>
     );
